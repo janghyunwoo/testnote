b9df1896fd9ffb283e8b044f44449425fb870ca4
Jang_home_n <Jang_home_n@Jang_home_n-PC>
2018-01-10 15:47:40 +0900

    노트북 test git~

---
+++ JavaWorkStation/src/soldesk/java/work/Test.java
@@ -0,0 +1,28 @@
+package soldesk.java.work;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Test {
+
+	
+	int a=1;
+	static int b=2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ddd");
+		Test1 aa = new Test1();
+		naa();
+	}
+	public static void naa() {
+		System.out.println("dd");
+		
+	}
+	
+	
+	public static class Test1{ 
+		
+		
+	}
+}
+
+
